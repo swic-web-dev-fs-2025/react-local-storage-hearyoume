2b768ed36bdde0ad903f6e7935a65afc8ef62727
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-11-07 10:10:15 -0600

    Replace App component with ToDoApp in main rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.jsx";
+import ToDoApp from "./ToDoApp.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ToDoApp />
   </StrictMode>
 );
